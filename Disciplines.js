fb9e3a10628b1ffa70a3c743fc1ee00972d5d0d4
Greg <greg@greg-Satellite-C855D.(none)>
2013-07-28 14:26:47 -0400

    Markets

---
+++ Disciplines.js
@@ -0,0 +1,74 @@
+// JavaScript adapted from 2011-Haverbeke.(124-128)
+
+Object.prototype.create = function() {
+
+// Stuff.
+// Things.
+
+};
+
+Object.prototype.extend = function(properties) {
+
+// Stuff.
+// Things.
+
+};
+
+var Item = {
+    construct: function(name)
+    inspect:   function()
+    kick:      function()
+    take:      function()
+    }
+};
+
+"Disciplines offer rules of the games that yield
+coordination in tasks in an otherwise messy world."
+
+"Our prototypical discipline is the INTERFACE
+discipline, since that has been analyzed in depth
+over the past three decades."
+
+Interface Discipline
+    - Valuation Order:  Quality
+    - Process:          Commit
+
+Council Discipline
+    - Valuation Order:  Prestige
+    - Process:          Mediate
+
+Arena Discipline
+    - Valuation Order:  Purity
+    - Process:          Select    
+
+Discipline > Interface
+Discipline > Council
+Discipline > Arena
+
+Meals as Social Processes.
+    - Sit-down urban dinner: Arena
+    - Church supper:         Council
+    - Cafeteria:             Interface
+
+Any equation must sustain three very distinct variables;
+the equation must be in contingency as well as in 
+ambiguity and ambage. (71). [Deserves treatment
+akin to that of information theory]
+
+Invokes chapter 7.3.2, in which this approach is called
+"Agency Calculus"
+ 
+// Practice JavaScript. Adapted from 2011-Haverbeke.
+
+// 43. Object Values
+var cat = {color: "gray", name: "Spot", size: 46};
+cat.size = 47;
+cat.size;
+
+delete cat.size;
+cat.size;
+
+var empty = {};
+empty.notReally = 1000;
+empty;
+
