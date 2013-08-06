316126d9cb47f50c881254802e7629611ff268e4
Greg <greg@greg-Satellite-C855D.(none)>
2013-08-06 11:11:58 -0400

    Small update

---
+++ Meta.rb
@@ -0,0 +1,21 @@
+module Meta
+
+module Intro end
+
+module MetaProgrammingRuby 
+
+def to_alphanumeric(s)
+  s.gsub /[^\w\s]/,''
+end
+
+require 'test/unit'
+
+
+
+end
+
+module MetaProgrammingInRails end
+
+module Appendixes end
+
+end
---
+++ RSpec.rb
@@ -0,0 +1,6 @@
+module RSpec
+
+
+
+
+end
