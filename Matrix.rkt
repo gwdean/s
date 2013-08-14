9a161a4a6fc9c69779b608993fc8ee06e3c46142
Greg <greg@greg-Satellite-C855D.(none)>
2013-08-14 11:54:39 -0400

    Updated Matrix.rkt

--- Matrix.rkt
+++ Matrix.rkt
@@ -31,3 +31,79 @@
          [i 0 (+ i 1)])
         ((= i rows) m)
       (vector-set! m i (make-vector columns)))))
+
+; matrix? checks to see if its argument is a matrix.
+; It is not foolproof, but works for most intents and purposes.
+(define matrix?
+  (lambda (x)
+    (and (vector? x)
+         (> (vector-length x) 0)
+         (vector? (vector-ref x 0)))))
+
+; matrix-rows returns the number of rows in a matrix.
+(define matrix-rows
+  (lambda (x)
+    (vector-length x)))
+
+; matrix-columns returns the number of columns in a matrix.
+(define matrix-columns
+  (lambda (x)
+    (vector-length (vector-ref x 0))))
+
+; matrix-ref returns the jth element of the ith row.
+(define matrix-ref
+  (lambda (m i j)
+    (vector-ref (vector-ref m i) j)))
+
+; matrix-set! changes the jth element of the ith row.
+(define matrix-set!
+  (lambda (m i j x)
+    (vector-set! (vector-ref m i) j x)))
+
+; mat-sca-mul multiplies a matrix by a scalar.
+(define mat-sca-mul
+  (lambda (m x)
+    (let* ([nr (matrix-rows m)]
+           [nc (matrix-columns m)]
+           [r (make-matrix nr nc)])
+      (do ([i 0 (+ i 1)])
+          ((= i nr) r)
+        (do ([j 0 (+ j 1)])
+            ((= j nc))
+          (matrix-set! r i j (* x (matrix-ref m i j))))))))
+
+; mat-mat-mul multiplies one matrix by another, after verifying
+; that the first matrix has as many columns as the second
+; matrix has rows.
+
+
+; type-error is called to complain when mul receives an invalid
+; type of argument
+(define type-error
+  (lambda (what)
+    (assertion-violation 'mul
+      "not a number or matrix"
+      what)))
+
+; match-error is called to complain when mul receives a pair of
+; incompatible arguments.
+(define match-error
+  (lambda (what1 what2)
+    (assertion-violation 'mul
+      "incompatible operands" what1
+      what2)))
+
+; mul is the generic matrix/scalar multiplication procedure
+(define mul
+  (lambda (x y)
+    (cond
+      [(number? x)
+       (cond
+         [(number? y) (* x y)]
+         [(matrix? y) (mat-sca-mul y x)]
+         [else (type-error y)])]
+      [(matrix? x)
+       (cond
+         [(number? y) (mat-sca-mul x y)]
+         [(matrix? y) (mat-mat-mul x y)]
+         [else (type-error x)])])))
