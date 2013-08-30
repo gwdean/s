6d0b1030308376bd609b22021a41f3c8a9453ba5
Greg <greg@greg-Satellite-C855D.(none)>
2013-08-29 21:40:30 -0400

    Updated Mind.org, etc

---
+++ Jq.js
@@ -0,0 +1,11 @@
+// Notes from "jQuery Cookbook" (2010-Lindley)
+
+{ 
+    "title": "Functional Differential Geometry",
+    "author": [
+	{ "name": "Jack Wisdom"},
+	{ "name": "Gerald Jay Sussman"}	
+    ],
+    "year": "2012",
+    "location": "Cambridge, MA"
+}
